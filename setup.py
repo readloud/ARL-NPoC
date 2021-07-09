b6811e2dedf4aaadf1da38a81f2a59561390ffd8
1c3z <icez1223@qq.com>
2021-07-09 13:09:46 +0800

    添加编码

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 
 import setuptools
 
-with open("README.md", "r") as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 from xing import __version__ as version
